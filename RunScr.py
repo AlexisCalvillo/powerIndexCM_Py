4a317fddb7cae1ca6b5458373245900151ece751
Alexis Calvillo <acalvillm@gmail.com>
2020-11-26 12:53:59 -0600

    Script local test

--- RunScr.py
+++ RunScr.py
@@ -1,7 +1,9 @@
-from Test import example
+import Test as tst
 import numpy as np
 import multiprocessing as mp
-a=np.random.uniform(0,100,50)
-print(a)
-example()
-print("Number of processors: ", mp.cpu_count())
+#a=np.random.uniform(0,100,50)
+#print(a)
+#tst.example()
+if __name__ == '__main__':
+    print("Number of processors: ", mp.cpu_count())
+    tst.test(5,4,0.5)

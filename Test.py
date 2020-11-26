c40ef09002e8881ac16031abae05cabf9e0ea36c
Alexis Calvillo <acalvillm@gmail.com>
2020-11-26 12:55:42 -0600

    Error correction, parallelize loop and format.

--- Test.py
+++ Test.py
@@ -5,6 +5,8 @@ from time import time
 import numpy as np
 import math as mt
 import multiprocessing as mp
+
+results =[]
 def example():
     q=.50
     vecVot=[20,25,25,30]
@@ -45,40 +47,40 @@ def calcIndexes(vecVot, q):
     [matrEcu,  solutionCM, eqcoal]=powerIndexCM(vecVot,q)
     finT=time()
     timeVec[0]=finT-iniT
-    indexVec[0]=solutionCM.x
+    indexVec[0]=solutionCM.x.tolist()
     
     #Holler Packel
     iniT=time()
     solutionHP=hollPack(vecVot,q)
     finT=time()
     timeVec[3]=finT-iniT
-    indexVec[3]=solutionCM.x
+    indexVec[3]=solutionHP
     
     #Shapley
     iniT=time()
     solutionSH=shapley(vecVot,q)
     finT=time()
     timeVec[1]=finT-iniT
-    indexVec[1]=solutionCM.x
+    indexVec[1]=solutionSH
     
     #DeeganPackel
     iniT=time()
     solutionDP=deegPack(vecVot,q)
     finT=time()
     timeVec[4]=finT-iniT
-    indexVec[4]=solutionCM.x
-    
+    indexVec[4]=solutionDP
+
     #Banzhaf
     iniT=time()
     solutionB=banzhaf(vecVot,q)
     finT=time()
     timeVec[2]=finT-iniT
-    indexVec[2]=solutionCM.x
+    indexVec[2]=solutionB
 
     return [timeVec, indexVec]
     
     
-def genAndCalc(playNum,q):
+def genAndCalc(i,playNum,q):
     """
     Generates a random vector of weights and calculate all the indexes of power an return the vector generated
     the time of excecution and the vector of power in this order
@@ -86,11 +88,13 @@ def genAndCalc(playNum,q):
     playNum: Numbers of players
     q: percent of the total weight (quota, o to 1)
     """
-    vecVot=mt.ceil(np.random.Uniform(0,1,playNum)*100)
+    vecVot=np.ceil(np.random.uniform(0,100,playNum)).tolist()
     [timeVec, indexVec]=calcIndexes(vecVot,q)
     return [vecVot,timeVec,indexVec]
 
-
+def collect_result(result):
+    global results
+    results.append(result)
 
 def test(itNum, playNum, q):
     """
@@ -100,13 +104,24 @@ def test(itNum, playNum, q):
     itNum: Vector of player weight
     q: percent of the total weight (quota, o to 1)
     """
+    vecVot=[]
+    timeVec=[]
+    indexVec=[]
+    results=[]
     pool = mp.Pool(mp.cpu_count())
-    inputsN = range(itNum)
-    for i in range(itNum):
-        [vecVot,timeVec,indexVec] = pool.map_async(genAndCalc, args=(playNum,q))
+    results = pool.starmap_async(genAndCalc,[(i, playNum, q) for i in range(itNum)]).get()
     
+    pool.close()
+
+    for x in results:
+        vecVot.append(x[0])
+        timeVec.append(x[1])
+        indexVec.append(x[2])
+    print("Vector de votantes")
     print(vecVot)
+    print("Vector de tiempos")
     print(timeVec)
+    print("Vector de índices")
     print(indexVec)
        
 
